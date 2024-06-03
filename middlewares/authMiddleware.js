38bd877441a4af538c02f2f2fd2f122c91a17c67
ikavyathota <146860492+ikavyathota@users.noreply.github.com>
2024-06-03 23:42:17 +0530

    Create authMiddleware.js

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'Token not provided' });
+    }
+
+    const token = authorization.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ error: 'Token expired' });
+        } else if (error.name === 'JsonWebTokenError') {
+            return res.status(401).json({ error: 'Invalid token' });
+        } else {
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+};
+
+module.exports = authMiddleware;
